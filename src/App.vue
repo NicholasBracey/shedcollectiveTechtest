3007d37abe49a9042872f021ae209d1ac884e639
Rad <radhikapatel@radhikas-mbp.home>
2019-02-03 20:58:19 +0000

    button dialog

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,66 @@
 <template>
-<v-app>
-
-  <template>
-    <v-app id="rating">
+<v-app id="rating">
       <v-content>
-        <v-container fluid fill-height>
-          <v-layout align-center justify-center>
+        <template>
+          <div class="text-xs-center">
+            <v-dialog v-model="dialog" width="658">
+              <v-btn slot="activator" color="red lighten-2" dark>
+                Click Me
+              </v-btn>
 
-            <v-card class="elevation-16 text-xs-center" width="658">
+              <v-container fluid fill-height>
+                <v-layout align-center justify-center>
 
-              <div class="mt-5">
-                <div class="rating-title">
-                  How did we do?
-                </div>
-                <div class="copy mt-2">
-                  Please let us know how your food delievery was.<br>It will really help us to keep improving our service!
-                </div>
-              </div>
+                  <v-card class="elevation-16 text-xs-center">
 
-              <div class="text-xs-center mt-4">
-                <div class="copy">
-                  How would you rate your food?
-                </div>
+                    <div class="mt-5">
+                      <div class="rating-title">
+                        How did we do?
+                      </div>
+                      <div class="copy mt-2">
+                        Please let us know how your food delievery was. It will really help us to keep improving our service!
+                      </div>
+                    </div>
 
-                <v-rating v-model="rating1" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
-              </div>
+                    <div class="text-xs-center mt-4">
+                      <div class="copy">
+                        How would you rate your food?
+                      </div>
 
-              <div class="text-xs-center mt-4">
-                <div class="copy">
-                  How would you rate your delivery driver?
-                </div>
+                      <v-rating v-model="rating1" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
+                    </div>
 
-                <v-rating v-model="rating2" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
-              </div>
+                    <div class="text-xs-center mt-4">
+                      <div class="copy">
+                        How would you rate your delivery driver?
+                      </div>
 
-              <div class="text-xs-center mt-4">
-                <div class="copy">
-                  How would you rate your overall experience?
-                </div>
+                      <v-rating v-model="rating2" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
+                    </div>
 
-                <v-rating v-model="rating3" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
-              </div>
+                    <div class="text-xs-center mt-4">
+                      <div class="copy">
+                        How would you rate your overall experience?
+                      </div>
 
-              <div class="mt-2">
-                <v-btn color="#0021FF" large>Submit feedback</v-btn>
-              </div>
+                      <v-rating v-model="rating3" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
+                    </div>
 
-            </v-card>
+                    <div class="mt-2">
+                      <v-btn color="#0021FF" large>Submit feedback</v-btn>
+                    </div>
 
-          </v-layout>
-        </v-container>
+                  </v-card>
+
+                </v-layout>
+              </v-container>
+
+            </v-dialog>
+          </div>
+        </template>
+
       </v-content>
     </v-app>
-  </template>
-</v-app>
 </template>
 
 <script>
@@ -73,7 +80,7 @@ export default {
   font-size: 21px;
 }
 
-.v-btn__content{
+.v-btn__content {
   color: white;
   text-transform: none;
 }
