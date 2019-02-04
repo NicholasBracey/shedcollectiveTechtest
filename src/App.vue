76f9a773386cd25eaca071e583dfd1da3bb81d2e
Nicholas Bracey <nbracey@twc-003184-a.local>
2019-02-04 19:00:05 +0000

    rating data display

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,146 @@
 <template>
-<v-app id="rating">
-      <v-content>
-        <template>
-          <div class="text-xs-center">
-            <v-dialog v-model="dialog" width="658">
-              <v-btn slot="activator" color="red lighten-2" dark>
-                Click Me
-              </v-btn>
+  <v-app id="rating">
+    <main>
+      <v-img class="img_rating" src="https://pbs.twimg.com/media/Cpg9zadVMAA48Bl.png"></v-img>
 
-              <v-container fluid fill-height>
-                <v-layout align-center justify-center>
+      <div class="text-xs-center">
+        <v-btn color="#FB9F44" @click="dialog = true">Show me what you got!</v-btn>
+      </div>
 
-                  <v-card class="elevation-16 text-xs-center">
+      <v-dialog v-model="dialog" transition="dialog-bottom-transition" width="659">
+        <v-container>
+          <v-card>
+            <v-icon @click="dialog = false">cancel</v-icon>
 
-                    <div class="mt-5">
-                      <div class="rating-title">
-                        How did we do?
-                      </div>
-                      <div class="copy mt-2">
-                        Please let us know how your food delievery was. It will really help us to keep improving our service!
-                      </div>
-                    </div>
+            <v-card-title class="justify-center rating__title pt-5">How did we do?</v-card-title>
+            <p
+              class="rating__copy mt-2 mr-4 ml-4"
+            >Please let us know how your food delievery was. It will really help us to keep improving our service!</p>
 
-                    <div class="text-xs-center mt-4">
-                      <div class="copy">
-                        How would you rate your food?
-                      </div>
+            <div class="text-xs-center mt-4">
+              <h2 class="rating__subhead">How would you rate your food?</h2>
+              <v-rating
+                v-model="foodRating"
+                color="yellow darken-3"
+                background-color="grey darken-1"
+                empty-icon="$vuetify.icons.ratingFull"
+                half-increments
+                hover
+                large
+              ></v-rating>
+            </div>
 
-                      <v-rating v-model="rating1" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
-                    </div>
+            <div class="text-xs-center mt-4">
+              <h2 class="rating__subhead">How would you rate your delivery driver?</h2>
 
-                    <div class="text-xs-center mt-4">
-                      <div class="copy">
-                        How would you rate your delivery driver?
-                      </div>
+              <v-rating
+                v-model="driverRating"
+                color="yellow darken-3"
+                background-color="grey darken-1"
+                empty-icon="$vuetify.icons.ratingFull"
+                half-increments
+                hover
+                large
+              ></v-rating>
+            </div>
 
-                      <v-rating v-model="rating2" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
-                    </div>
+            <div class="text-xs-center mt-4">
+              <h2 class="rating__subhead">How would you rate your overall experience?</h2>
 
-                    <div class="text-xs-center mt-4">
-                      <div class="copy">
-                        How would you rate your overall experience?
-                      </div>
+              <v-rating
+                v-model="overallRating"
+                color="yellow darken-3"
+                background-color="grey darken-1"
+                empty-icon="$vuetify.icons.ratingFull"
+                half-increments
+                hover
+                large
+              ></v-rating>
+            </div>
 
-                      <v-rating v-model="rating3" color="yellow darken-3" background-color="grey darken-1" empty-icon="$vuetify.icons.ratingFull" half-increments hover large></v-rating>
-                    </div>
-
-                    <div class="mt-2">
-                      <v-btn color="#0021FF" large>Submit feedback</v-btn>
-                    </div>
-
-                  </v-card>
-
-                </v-layout>
-              </v-container>
-
-            </v-dialog>
-          </div>
-        </template>
-
-      </v-content>
-    </v-app>
+            <div class="text-xs-center pb-5">
+              <v-btn @click="handleSubmit" color="#0021FF" large>Submit feedback</v-btn>
+            </div>
+          </v-card>
+        </v-container>
+      </v-dialog>
+    </main>
+  </v-app>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   data: () => ({
-    rating1: 4
-  })
-}
+    foodRating: 3,
+    driverRating: 3,
+    overallRating: 3,
+    dialog: false
+  }),
+  methods: {
+    handleSubmit() {
+      alert(
+        `food rating: ${this.foodRating}, driver rating: ${
+          this.driverRating
+        }, overall rating: ${this.overallRating}`
+      );
+    }
+  }
+};
 </script>
 
-<style>
-.rating-title {
-  font-size: 31px;
+<style scoped lang="postcss">
+.container {
+  box-shadow: 0;
 }
 
-.copy {
-  font-size: 21px;
+.v-dialog:not(.v-dialog--fullscreen) {
+  box-shadow: none !imporant;
 }
 
-.v-btn__content {
+.rating {
+  font-family: 'Helvetica', sans-serif;
+}
+
+.rating__content {
+  padding: 1em 2em;
+}
+
+.rating__title {
+  font-size: 2.5em;
+  font-weight: bold;
+}
+
+.rating__subhead {
+  padding: 1em 2em;
+}
+
+.rating__copy {
+  font-size: 1.5em;
+  text-align: center;
+}
+
+.v-icon {
+  position: absolute;
+  top: -20px;
+  right: -10px;
+  font-size: 60px;
+  color: #000;
+  cursor: pointer;
+}
+
+.v-btn {
   color: white;
   text-transform: none;
+  font-weight: bold;
+  padding: 1.5em 2.5em;
+  line-height: 3px;
+  margin: 1em;
+  text-align: center;
+}
+
+.img_rating {
+  margin: 0 auto;
+  width: 300px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import Vuetify from 'vuetify'
 // index.js or main.js
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
@@ -14,7 +13,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: { App },
   template: '<App/>'
 })
